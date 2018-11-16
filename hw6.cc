ea67f99f11b33494949fae8e9e326cc78e56f8b5
Ty Sabs <tysabs@gmail.com>
2018-11-15 23:05:14 -0600

    Loop through whole file

--- hw6.cc
+++ hw6.cc
@@ -15,20 +15,20 @@ using std::endl;
 using std::list;
 using std::ifstream;
 
-const int HOWOFTEN = 1;
 const int ONE_MB = 1024 * 1024;
 
 void printAvailableBlocks(list<MemoryBlock> &availableBlocks) {
   cerr << "List of available blocks" << endl;
 
   int totalSize = 0;
-  for (MemoryBlock mb : availableBlocks) {
-    mb.printBlockInfo();
-    totalSize += mb.getSize();
+  list<MemoryBlock>::iterator it;
+  for (it = availableBlocks.begin(); it != availableBlocks.end(); it++) {
+    it->printBlockInfo();
+    totalSize += it->getSize();
   }
 
   cerr << "Total size of the list = " << totalSize << endl << endl;
-}
+} // end printAvailableBlocks
 
 void printBlocksInUse(list<MemoryBlock> &inUseBlocks) {
   cerr << "List of blocks in use" << endl;
@@ -37,16 +37,17 @@ void printBlocksInUse(list<MemoryBlock> &inUseBlocks) {
   if (inUseBlocks.size() <= 0) {
     cerr << "(none)" << endl;
   } else {
-    for (MemoryBlock mb : inUseBlocks) {
-      mb.printUsageInfo();
-      totalSize += mb.getSize();
+    list<MemoryBlock>::iterator it;
+    for (it = inUseBlocks.begin(); it != inUseBlocks.end(); it++) {
+      it->printBlockInfo();
+      totalSize += it->getSize();
     }
   }
 
   cerr << "Total Size of the list = " << totalSize << endl << endl;
-}
+} // end printBlocksInUse
 
-void allocateMemory(list<MemoryBlock> &availableBlocks, MemoryBlock &processBlock) {
+bool allocateMemory(list<MemoryBlock> &availableBlocks, MemoryBlock &processBlock) {
   processBlock.printProcessInfo();
 
   list<MemoryBlock>::iterator it;
@@ -58,34 +59,53 @@ void allocateMemory(list<MemoryBlock> &availableBlocks, MemoryBlock &processBloc
       int newSize = it->getSize() - processBlock.getSize();
       it->setSize(newSize);
 
-
       int newAddress = it->getStartAddress() + processBlock.getSize();
       it->setStartAddress(newAddress);
       cerr << "Success in allocating a block" << endl << endl;
-      break;
+      return true;
     }
   }
-}
+
+  return false;
+} // end allocateMemory
 
 void deallocateMemory(list<MemoryBlock> &availableBlocks, MemoryBlock &terminateBlock) {
-  list<MemoryBlock>::iterator it;
   int blockOffset = terminateBlock.getStartAddress() + terminateBlock.getSize();
+
+  list<MemoryBlock>::iterator it, it2;
+  bool isNotFirstLoop = false;
   for (it = availableBlocks.begin(); it != availableBlocks.end(); it++) {
     if (it->getStartAddress() == blockOffset) {
+      cerr << "Merging two blocks at " << terminateBlock.getStartAddress();
+      cerr << " and " << it->getStartAddress() << endl;
 
-      cerr << "Merging two blocks at " << terminateBlock.getStartAddress() << " and "
-            << it->getStartAddress() << endl;
-
-      it->setStartAddress(blockOffset);
+      it->setStartAddress(terminateBlock.getStartAddress());
       int newSize = it->getSize() + terminateBlock.getSize();
       it->setSize(newSize);
-
-      break;
     }
-  }
+
+    if (isNotFirstLoop && it2->getStartAddress() + it2->getSize() == it->getStartAddress()) {
+      int newSize = it->getSize() + it2->getSize();
+
+      if (newSize > 4 * ONE_MB) {
+        continue;
+      } else {
+        cerr << "Merging two blocks at " << it2->getStartAddress();
+        cerr << " and " << it->getStartAddress() << endl;
+
+        it->setStartAddress(it2->getStartAddress());
+        it->setSize(newSize);
+
+        availableBlocks.erase(it);
+      }
+    }
+
+    isNotFirstLoop = true;
+    it2 = it;
+  } // end for loop
 
   cerr << endl;
-}
+} // end deallocateMemory
 
 
 /***********************************************************************
@@ -145,43 +165,56 @@ int main(int argc, char *argv[]) {
   Avail.push_back(block5);
 
 
-  if (*argv[1] == 'B') {
-    cerr << "B detected" << endl;
-    printAvailableBlocks(Avail);
-  } else if (*argv[1] == 'F') {
+  if (*argv[1] == 'F' || 'B') {
     cerr << "Simulation of Memory Management using the First-Fit algorithm" << endl << endl;
     cerr << "Beginning of the run" << endl << endl;
     printAvailableBlocks(Avail);
     printBlocksInUse(InUse);
 
-    int foo = 0;
-
     char transactionType;
     string pid, bid, endLine;
     int pSize;
     MemoryBlock newBlock;
+    bool success;
 
     infile >> transactionType;
 
-    while (infile && foo < 3) {
+    while (infile) {
       list<MemoryBlock>::iterator removeIterator;
-
 
       switch (transactionType) {
         case 'L':
+        case 'A':
           infile >> pid;
           infile >> pSize;
           infile >> bid;
 
           newBlock = MemoryBlock(pSize, pid, bid);
 
-          allocateMemory(Avail, newBlock);
-          InUse.push_back(newBlock);
+          success = allocateMemory(Avail, newBlock);
+          if (success) { InUse.push_back(newBlock); }
+
           printAvailableBlocks(Avail);
           printBlocksInUse(InUse);
           break;
+        // case 'A':
+        //   infile >> pid;
+        //   infile >> pSize;
+        //   infile >> bid;
+        //
+        //   newBlock = MemoryBlock(pSize, pid, bid);
+        //
+        //   success = allocateMemory(Avail, newBlock);
+        //   if (success) { InUse.push_back(newBlock); }
+        //
+        //   printAvailableBlocks(Avail);
+        //   printBlocksInUse(InUse);
+        //   break;
+        case 'D':
         case 'T':
           infile >> pid;
+
+          if (transactionType == 'D') { infile >> pSize; }
 
           cerr << "Transaction: request to terminate process " << pid << endl;
 
@@ -195,12 +228,15 @@ int main(int argc, char *argv[]) {
           }
 
           break;
-
+        case '?':
+          cerr << endl << "Terminating program." << endl << endl;
+          return 0;
+          break;
         default:
           cerr << "Unhandled Char" << endl;
+          break;
       } // end switch
 
-      foo++;
       infile >> transactionType;
     } // end while loop
 
@@ -211,4 +247,4 @@ int main(int argc, char *argv[]) {
 
 
   return 0;
-}
+} // end main
