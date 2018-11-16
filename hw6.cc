f6b88e84eca521ea0c78b772bb659dbab1816b5e
Ty Sabs <tysabs@gmail.com>
2018-11-15 23:34:36 -0600

    Add comments

--- hw6.cc
+++ hw6.cc
@@ -17,6 +17,16 @@ using std::ifstream;
 
 const int ONE_MB = 1024 * 1024;
 
+/***********************************************************************
+ * printAvailableBlocks function
+ *
+ * Arguments(1):
+ *   1. availableBlocks - list of available blocks
+ *
+ * Returns: void
+ *
+ * Use: Prints the contents of the Avail list
+ ***********************************************************************/
 void printAvailableBlocks(list<MemoryBlock> &availableBlocks) {
   cerr << "List of available blocks" << endl;
 
@@ -30,10 +40,22 @@ void printAvailableBlocks(list<MemoryBlock> &availableBlocks) {
   cerr << "Total size of the list = " << totalSize << endl << endl;
 } // end printAvailableBlocks
 
+
+/***********************************************************************
+ * printBlocksInUse function
+ *
+ * Arguments(1):
+ *   1. inUseBlocks - list of blocks being used
+ *
+ * Returns: void
+ *
+ * Use: Prints the contents of the InUse list
+ ***********************************************************************/
 void printBlocksInUse(list<MemoryBlock> &inUseBlocks) {
   cerr << "List of blocks in use" << endl;
   int totalSize = 0;
 
+  // Check if inUse is empty, otherwise print its contents and add to total
   if (inUseBlocks.size() <= 0) {
     cerr << "(none)" << endl;
   } else {
@@ -42,11 +64,22 @@ void printBlocksInUse(list<MemoryBlock> &inUseBlocks) {
       it->printBlockInfo();
       totalSize += it->getSize();
     }
-  }
+  } // end else
 
   cerr << "Total Size of the list = " << totalSize << endl << endl;
 } // end printBlocksInUse
 
+/***********************************************************************
+ * allocateMemory function
+ *
+ * Arguments(2):
+ *   1. availableBlocks - list of available blocks
+ *   2. processBlock - block to be allocated
+ *
+ * Returns: void
+ *
+ * Use: allocates a block of a memory in the available blocks list
+ ***********************************************************************/
 bool allocateMemory(list<MemoryBlock> &availableBlocks, MemoryBlock &processBlock) {
   processBlock.printProcessInfo();
 
@@ -54,53 +87,74 @@ bool allocateMemory(list<MemoryBlock> &availableBlocks, MemoryBlock &processBloc
   for (it = availableBlocks.begin(); it != availableBlocks.end(); it++) {
     if (processBlock.getSize() < it->getSize()) {
       cerr << "Found a block of size " << it->getSize() << endl;
+
+      // Set process's starting address
       processBlock.setStartAddress(it->getStartAddress());
 
+      // Resize existing block of memory
       int newSize = it->getSize() - processBlock.getSize();
       it->setSize(newSize);
 
+      // Set the starting address of resized block of memory
       int newAddress = it->getStartAddress() + processBlock.getSize();
       it->setStartAddress(newAddress);
       cerr << "Success in allocating a block" << endl << endl;
       return true;
     }
-  }
+  } // end for loop
 
   return false;
 } // end allocateMemory
 
+
+/***********************************************************************
+ * deallocateMemory function
+ *
+ * Arguments(2):
+ *   1. availableBlocks - list of available blocks
+ *   2. terminateBlock - block to be allocated
+ *
+ * Returns: void
+ *
+ * Use: allocates a block of a memory in the available blocks list
+ ***********************************************************************/
 void deallocateMemory(list<MemoryBlock> &availableBlocks, MemoryBlock &terminateBlock) {
   int blockOffset = terminateBlock.getStartAddress() + terminateBlock.getSize();
 
   list<MemoryBlock>::iterator it, it2;
   bool isNotFirstLoop = false;
   for (it = availableBlocks.begin(); it != availableBlocks.end(); it++) {
+    // If block is the block below terminated block, delete  merge and terminated block
     if (it->getStartAddress() == blockOffset) {
       cerr << "Merging two blocks at " << terminateBlock.getStartAddress();
       cerr << " and " << it->getStartAddress() << endl;
 
+      // Set merged block's address
       it->setStartAddress(terminateBlock.getStartAddress());
+
+      // Set merged block's size
       int newSize = it->getSize() + terminateBlock.getSize();
       it->setSize(newSize);
     }
 
+    // If last block's ending matches this block's starting, merge the two blocks
     if (isNotFirstLoop && it2->getStartAddress() + it2->getSize() == it->getStartAddress()) {
       int newSize = it->getSize() + it2->getSize();
 
-      if (newSize > 4 * ONE_MB) {
-        continue;
-      } else {
+      // Check to make sure newSize is less than 4MB
+      if (newSize < 4 * ONE_MB) {
         cerr << "Merging two blocks at " << it2->getStartAddress();
         cerr << " and " << it->getStartAddress() << endl;
 
+        // Set starting address and size to former's address and merged size
         it->setStartAddress(it2->getStartAddress());
         it->setSize(newSize);
 
         availableBlocks.erase(it);
-      }
-    }
+      } // end if less than 4MB
+    } // end if matched
 
-    isNotFirstLoop = true;
+    isNotFirstLoop = true; // prevents fault
     it2 = it;
   } // end for loop
 
@@ -135,7 +189,6 @@ int main(int argc, char *argv[]) {
   Avail = list<MemoryBlock>();
 
   MemoryBlock block1, block2, block3, block4, block5;
-
   const int size1 = 1 * ONE_MB,
             size2 = 2 * ONE_MB,
             size3 = 2 * ONE_MB,
@@ -144,6 +197,7 @@ int main(int argc, char *argv[]) {
 
   int blockIterator = 3 * ONE_MB;
 
+  // Instantiate starting memory blocks
   block1 = MemoryBlock((blockIterator), size1);
   blockIterator += size1;
 
@@ -158,13 +212,14 @@ int main(int argc, char *argv[]) {
 
   block5 = MemoryBlock((blockIterator), size5);
 
+  // Add memory blocks to Avail list
   Avail.push_back(block1);
   Avail.push_back(block2);
   Avail.push_back(block3);
   Avail.push_back(block4);
   Avail.push_back(block5);
 
-
+  // This program only uses First-Fit algorithm
   if (*argv[1] == 'F' || 'B') {
     cerr << "Simulation of Memory Management using the First-Fit algorithm" << endl << endl;
     cerr << "Beginning of the run" << endl << endl;
@@ -177,9 +232,11 @@ int main(int argc, char *argv[]) {
     MemoryBlock newBlock;
     bool success;
 
+    // Prime read
     infile >> transactionType;
 
-    while (infile) {
+    // Loop through the file's contents
+    while (infile && transactionType != '?') {
       list<MemoryBlock>::iterator removeIterator;
 
       switch (transactionType) {
@@ -197,19 +254,6 @@ int main(int argc, char *argv[]) {
           printAvailableBlocks(Avail);
           printBlocksInUse(InUse);
           break;
-        // case 'A':
-        //   infile >> pid;
-        //   infile >> pSize;
-        //   infile >> bid;
-        //
-        //   newBlock = MemoryBlock(pSize, pid, bid);
-        //
-        //   success = allocateMemory(Avail, newBlock);
-        //   if (success) { InUse.push_back(newBlock); }
-        //
-        //   printAvailableBlocks(Avail);
-        //   printBlocksInUse(InUse);
-        //   break;
         case 'D':
         case 'T':
           infile >> pid;
@@ -225,7 +269,7 @@ int main(int argc, char *argv[]) {
               cerr << "Success in terminating a process" << endl << endl;
               break;
             }
-          }
+          } // end for loop
 
           break;
         case '?':
--- memoryblock.cc
+++ memoryblock.cc
@@ -12,15 +12,31 @@
 using std::cerr;
 using std::endl;
 
-MemoryBlock::MemoryBlock() {
 
-}
-
+/***********************************************************************
+ * MemoryBlock constructor
+ *
+ * Arguments(2):
+ *   1. address - starting address
+ *   2. _size - size of the block
+ *
+ * Use: Creates a memory block without a process or block ID
+ ***********************************************************************/
 MemoryBlock::MemoryBlock(int address, int _size) {
   startAddress = address;
   size = _size;
 }
 
+/***********************************************************************
+ * MemoryBlock constructor
+ *
+ * Arguments(3):
+ *   1. _size - size of the block
+ *   2. pid - process ID
+ *   3. bid - block ID
+ *
+ * Use: Creates a memory block with a process and block ID
+ ***********************************************************************/
 MemoryBlock::MemoryBlock(int _size, string pid, string bid) {
   startAddress = -1;
   size = _size;
@@ -28,31 +44,96 @@ MemoryBlock::MemoryBlock(int _size, string pid, string bid) {
   blockId = bid;
 }
 
+/***********************************************************************
+ * MemoryBlock.getSize method
+ *
+ * Arguments(0):
+ *
+ * Returns: the block's size
+ *
+ * Use: gets the block size
+ ***********************************************************************/
 int MemoryBlock::getSize() {
   return size;
 }
 
+/***********************************************************************
+ * MemoryBlock.setSize method
+ *
+ * Arguments(1):
+ *  1. value - the new size value
+ *
+ * Returns: void
+ *
+ * Use: sets the block size
+ ***********************************************************************/
 void MemoryBlock::setSize(int value) {
   size = value;
 }
 
+/***********************************************************************
+ * MemoryBlock.getStartAddress method
+ *
+ * Arguments(0):
+ *
+ * Returns: the block's starting address
+ *
+ * Use: gets the block's starting address
+ ***********************************************************************/
 int MemoryBlock::getStartAddress() {
   return startAddress;
 }
 
+/***********************************************************************
+ * MemoryBlock.setStartingAddress method
+ *
+ * Arguments(1):
+ *  1. value - the new starting address
+ *
+ * Returns: void
+ *
+ * Use: sets the block's starting address
+ ***********************************************************************/
 void MemoryBlock::setStartAddress(int value) {
   startAddress = value;
 }
 
+/***********************************************************************
+ * MemoryBlock.getProcessId method
+ *
+ * Arguments(0):
+ *
+ * Returns: the block's processID
+ *
+ * Use: gets the block's process ID
+ ***********************************************************************/
 string MemoryBlock::getProcessId() {
   return processId;
 }
 
+/***********************************************************************
+ * MemoryBlock.printBlockInfo method
+ *
+ * Arguments(0):
+ *
+ * Returns: void
+ *
+ * Use: prints the block info including address and size
+ ***********************************************************************/
 void MemoryBlock::printBlockInfo() {
   cerr << "Start Address = " << startAddress << " ";
   cerr << "Size = " << size << endl;
 }
 
+/***********************************************************************
+ * MemoryBlock.printUsageInfo method
+ *
+ * Arguments(0):
+ *
+ * Returns: void
+ *
+ * Use: prints the usage info including address, size, processId, and blockId
+ ***********************************************************************/
 void MemoryBlock::printUsageInfo() {
   cerr << "Start address = " << startAddress << " ";
   cerr << "Size = " << size << " ";
@@ -60,6 +141,15 @@ void MemoryBlock::printUsageInfo() {
   cerr << "Block ID = " << blockId << endl;
 }
 
+/***********************************************************************
+ * MemoryBlock.printProcessInfo method
+ *
+ * Arguments(0):
+ *
+ * Returns: void
+ *
+ * Use: prints the process info including size, processId, and blockId
+ ***********************************************************************/
 void MemoryBlock::printProcessInfo() {
   cerr << "Transaction: request to load process " << processId;
   cerr << ", block ID " << blockId;
